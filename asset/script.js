97c04dff0e040e39f5b1e8fd4500a7e9f9f6437f
rogelio martinez <rogelio.martinezsantiago1@gmail.com>
2021-06-16 09:26:40 -0700

    pruebas con javascript

---
+++ asset/script.js
@@ -0,0 +1,24 @@
+
+
+var dato1 = 24;
+var dato2 = "hola";
+var dato3 = "false";
+var dato4 = "5.37";
+var dato5 = true;
+var dato6 = null;
+var dato7 = "hoy es un lindo dia";
+var dato8 = 829;
+var dato9 = "null";
+var dato10 = false;
+
+console.log(typeof(dato1));
+console.log(typeof(dato2));
+console.log(typeof(dato3));
+console.log(typeof(parseFloat(dato4)), parseFloat(dato4) );
+console.log(typeof(dato5));
+console.log(typeof(dato6));
+console.log(typeof(dato7));
+console.log(typeof(dato8));
+console.log(typeof(dato9));
+console.log(typeof(dato10));
+console.log(typeof(dato11));
---
+++ asset/variables.js
@@ -0,0 +1,21 @@
+var variable = "soy lavariable 1 ";
+
+let variable = "soy la variable 2";
+
+const variable_constante = "12345";
+
+
+
+function varTest(){
+    let variable3 = "soy la variable 3";
+    var x = 31;
+    if(true){
+        var x = 71;
+        console.log(x);
+    }
+
+    console.log(x);
+}
+
+
+console.log(variable3);
